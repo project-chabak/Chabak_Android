76fdc87eaa3ea01a37aab130b6c5bff78445807e
yeonghyeonSeok <syh4834@naver.com>
2020-10-29 23:00:20 +0900

    회원가입 세부설정(비밀번호체크)

--- app/src/main/java/com/syh4834/chabak/SignupActivity.java
+++ app/src/main/java/com/syh4834/chabak/SignupActivity.java
@@ -2,9 +2,15 @@ package com.syh4834.chabak;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +18,14 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SignupActivity extends AppCompatActivity {
     ImageView imgBack;
     Button btnSignup;
+    EditText edtNickname;
+    EditText edtId;
+    EditText edtPw;
+    EditText edtPwCheck;
+    RadioGroup rgGender;
+    EditText edtBirth;
+    TextView tvPwCheckError;
+    ImageView imgPwCheck;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +33,14 @@ public class SignupActivity extends AppCompatActivity {
 
         imgBack = findViewById(R.id.img_back);
         btnSignup = findViewById(R.id.btn_signup);
+        edtNickname = findViewById(R.id.edt_nickname);
+        edtId = findViewById(R.id.edt_id);
+        edtPw = findViewById(R.id.edt_pw);
+        edtPwCheck = findViewById(R.id.edt_pw_check);
+        rgGender = findViewById(R.id.rg_gender);
+        edtBirth = findViewById(R.id.edt_birth);
+        tvPwCheckError = findViewById(R.id.tv_pw_check_error);
+        imgPwCheck = findViewById(R.id.img_pw_check);
 
         imgBack.setOnClickListener(l -> {
             Intent intent = new Intent(this, InitialActivity.class);
@@ -26,10 +48,45 @@ public class SignupActivity extends AppCompatActivity {
             finish();
         });
 
+        edtPwCheck.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(edtPw.getText().toString().equals(edtPwCheck.getText().toString())) {
+                    tvPwCheckError.setVisibility(View.GONE);
+                } else {
+                    tvPwCheckError.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(edtPw.getText().toString().equals(edtPwCheck.getText().toString())) {
+                    imgPwCheck.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
         btnSignup.setOnClickListener(l -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
+            int gender = 0;
+            String nickname = edtNickname.getText().toString();
+            String id = edtId.getText().toString();
+            String pw = edtPw.getText().toString();
+            String pwCheck = edtPwCheck.toString();
+            gender = rgGender.getCheckedRadioButtonId();
+            String birth = edtBirth.toString();
+
+            if (nickname.isEmpty() || id.isEmpty() || pw.isEmpty() || pwCheck.isEmpty() || gender == 0 || birth.isEmpty()) {
+                Toast.makeText(this, "회원가입 조건에 맞게 모두 채워주세요", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
 
     }
