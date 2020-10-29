ad5bc76624657fec938c16902c45844e258fbc6d
yeonghyeonSeok <syh4834@naver.com>
2020-10-29 13:16:03 +0900

    회원가입 화면구성

---
+++ app/src/main/java/com/syh4834/chabak/InitialActivity.java
@@ -0,0 +1,25 @@
+package com.syh4834.chabak;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InitialActivity extends AppCompatActivity {
+    Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+
+        btnSignup = findViewById(R.id.btn_signup);
+
+        btnSignup.setOnClickListener(l -> {
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+        });
+    }
+}
